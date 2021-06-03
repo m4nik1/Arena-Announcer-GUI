140a9c9f93da76dcfa80c5098dddd65ea392cee2
M4nik <maniksoomro@gmail.com>
2021-06-02 23:55:23 -0400

    First Commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../CSS/content.css"
+
+
+export default function Home()
+{
+    return (
+        <div className="content">
+          <h1>Welcome to Fight Announcer GUI</h1>
+        </div>
+    )
+}
