f100487cd4ccbffb36e805f121cdb4986910bb7d
M4nik <maniksoomro@gmail.com>
2021-06-17 13:51:56 -0400

    Changing requests to console.log

--- src/other/requests.js
+++ src/other/requests.js
@@ -1,17 +1,4 @@
-const axios = require("axios");
-
-
 export default function postRequest()
 {
-    axios.post("localhost:8080", {
-        testing: "pogchamp?"
-    })
-        .then(function (res) {
-            console.log(res);
-        })
-    
-        .catch(function(err) {
-            console.error(err);
-            console.log("ERROR has occurred");
-        })
+    console.log("Making a request")
 }
